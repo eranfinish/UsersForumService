0b290869729e182c81eabf1aac18def3306d4808
Eran Finish <finishe@gmail.com>
2024-09-22 06:59:08 +0300

    new commit

--- UsersForum/UsersForumClient/users-forum-app/src/app/store/actions/loading.action.ts
+++
@@ -1,4 +0,0 @@
-import { createAction } from '@ngrx/store';
-
-export const startLoading = createAction('[Loading] Start Loading');
-export const stopLoading = createAction('[Loading] Stop Loading');
--- UsersForum/UsersForumClient/users-forum-app/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-
-export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:7240/api',
-  hubUrl: 'https://localhost:7240/forumHub'
-
-};
-
