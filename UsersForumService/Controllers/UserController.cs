006e7c33ab702af563a889cb473c5be84586376f
Eran Finish <finishe@gmail.com>
2024-11-07 19:07:26 +0200

    update all

---
+++ UsersForumService/Controllers/UserController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UsersForumService.DAL.models;
+
+
+namespace UsersForumService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public IActionResult Register(User user)
+        {
+
+        }
+
+        [HttpPost("Login")]
+        public IActionResult Login(string username, string password)
+        {
+            try
+            {
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+return BadRequest();
+                throw;
+            }
+
+        }
+
+        private string GenerateJwtToken(string userId, string username)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("your_jwt_secret_here"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+            new Claim(JwtRegisteredClaimNames.Sub, userId),
+            new Claim(JwtRegisteredClaimNames.UniqueName, username),
+           
+        };
+
+            var token = new JwtSecurityToken(
+                issuer: "yourIssuer",
+                audience: "yourAudience",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [HttpGet("Logout")]
+        [Authorize]
+        public IActionResult Logout()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+          
+        }
+
+        
+    }
+}
